d4771a61aabe003ac8d9b1d2d91e25b2c458ed57
Dongyi Wang <dwan0067@student.monash.edu>
2025-07-31 18:19:03 +1000

    Efolio task 1.1 & 1.2

---
+++ vue-project/src/components/MyName.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="my-name">
+    <h2>Hello, my name is Dongyi Wang!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName'
+}
+</script>
+
+<style scoped>
+.my-name {
+  margin-top: 20px;
+  color: #42b983;
+}
+</style>
